c0b0f562f932e6826f1b972cfe3e4bd5e3439984
Andrey <andrejsatykov@yandex.ru>
2022-12-04 10:51:05 +0300

    Cube of Numbers Task 003

---
+++ Task_003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+void TablePOW2(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    int count = 1;
+    while (count <= number)
+    {
+        double pow = Math.Pow(count, 3);
+        Console.Write(pow + " ");
+        count++;
+    }
+    Console.WriteLine();
+}
+
+TablePOW2("Введите число N: ");
